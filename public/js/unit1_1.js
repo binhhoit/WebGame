ae93eec2f7d232088d70428811767a24c6835405
KIEU_DUYEN\Kiều Duyên <kieuduyen1809@gmail.com>
2017-08-03 23:01:20 +0700

    update game unit 1-1 1-2

--- public/js/unit1_1.js
+++ public/js/unit1_1.js
@@ -1,105 +1,97 @@
-
-var game = new Phaser.Game(1024, 720, Phaser.CANVAS, 'game_div', { preload: preload, create: create });
-
+var game = new Phaser.Game(1366, 768, Phaser.CANVAS, 'game_div', { preload: preload, create: create });
 
 var scoretext;
 var diem = 0;
 
-function preload () {
-	
-	game.scale.forceOrientation(false, true);
-	
-    game.load.image('carot1', 'assets/images/unit1_game1/carot_1.png');
-	game.load.image('carot2', 'assets/images/unit1_game1/carot_2.png');
-	game.load.image('carot3', 'assets/images/unit1_game1/carot_2.png');
-	game.load.image('gio', 'assets/images/unit1_game1/gio.png');
-	game.load.image('btn','assets/images/unit1_game1/ic_next.png');
+function preload() {
 
-	game.load.audio('music','assets/sounds/unit1_1/score.mp3');
-	game.load.audio('music1','assets/sounds/unit1_1/fail1.mp3');
-	game.load.audio('music3','assets/sounds/unit1_1/gioi_qua.mp3');
-	game.load.audio('music4','assets/sounds/unit1_1/hoan_ho.mp3');
-	game.load.audio('music5','assets/sounds/unit1_1/yeah.mp3');
-	game.load.audio('music6','assets/sounds/unit1_1/1_Game1.mp3');
-	game.load.audio('music7','assets/sounds/unit1_1/fail1.mp3');
-	
-	
-	game.load.image('background','assets/images/unit1_game1/bg.png');
-	
+	game.scale.forceOrientation(false, true);
+
+	game.load.image('carrot1', 'assets/images/unit1_game1/carrot_1.png');
+	game.load.image('carrot2', 'assets/images/unit1_game1/carrot_2.png');
+
+	game.load.image('basket', 'assets/images/unit1_game1/basket.png');
+	game.load.image('btnNext', 'assets/images/unit1_game1/ic_button_next.png');
+	game.load.image('btnRestart', 'assets/images/unit1_game1/ic_button_restart.png');
+
+	game.load.audio('music', 'assets/sounds/unit1_1/score.mp3');
+	game.load.audio('music1', 'assets/sounds/unit1_1/fail1.mp3');
+	game.load.audio('music3', 'assets/sounds/unit1_1/gioi_qua.mp3');
+	game.load.audio('music4', 'assets/sounds/unit1_1/hoan_ho.mp3');
+	game.load.audio('music5', 'assets/sounds/unit1_1/yeah.mp3');
+	game.load.audio('music6', 'assets/sounds/unit1_1/1_Game1.mp3');
+	game.load.audio('music7', 'assets/sounds/unit1_1/fail1.mp3');
+
+	game.load.image('background', 'assets/images/unit1_game1/bg.png');
+
 }
 
 
 function create() {
-	var background = game.add.sprite(0, 0, 'background');
-	//background.scale.setTo(0.5, 0.5);
-	var style = { font: "30px r0c0iLinotte", fill: "#000"};
-  	//scoretext = game.add.text(0, 10, '0',style);
-	
-	music6 = game.add.audio('music6');
-    music6.onDecoded.add(start, this);
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.world.setBounds(0, 0, 1366, 768);
+	var background = game.add.tileSprite(0, 0, 1366, 768, 'background');
+	//background.scale.setTo(0.7, 0.9);
+	var style = { font: "30px r0c0iLinotte", fill: "#000" };
+	//scoretext = game.add.text(0, 10, '0',style);
 
-	
-	
-    var image = game.add.sprite(200, 400, 'carot1');
-    image.scale.setTo(0.5,0.5);
-    image.inputEnabled = true;
+	music6 = game.add.audio('music6');
+	music6.onDecoded.add(start, this);
+
+	var image = game.add.sprite(200, 400, 'carrot1');
+	image.scale.setTo(0.5, 0.5);
+	image.inputEnabled = true;
 
 	image.events.onInputDown.add(score, this);
 	image.events.onInputDown.add(action, this);
 	image.events.onInputDown.add(animation, this);
 	image.events.onInputDown.add(process1, this);
-	
 
-	
-		
-	
-	var image1 = game.add.sprite(350, 400, 'carot2');
-    image1.scale.setTo(0.5,0.5);
-    image1.inputEnabled = true;
+	var image1 = game.add.sprite(350, 400, 'carrot2');
+	image1.scale.setTo(0.5, 0.5);
+	image1.inputEnabled = true;
 
 	image1.events.onInputDown.add(fail, this);
 	image1.events.onInputDown.add(action1, this);
 	image1.events.onInputDown.add(animation3, this);
-	
 
-	
-	var image2 = game.add.sprite(550, 400, 'carot1');
-    image2.scale.setTo(0.5,0.5);
-    image2.inputEnabled = true;
+	var image2 = game.add.sprite(550, 400, 'carrot1');
+	image2.scale.setTo(0.5, 0.5);
+	image2.inputEnabled = true;
 
 	image2.events.onInputDown.add(score1, this);
 	image2.events.onInputDown.add(action, this);
 	image2.events.onInputDown.add(animation1, this);
 	image2.events.onInputDown.add(process1, this);
 
-	
-	
-	var image3 = game.add.sprite(750, 400, 'carot3');
-    image3.scale.setTo(0.5,0.5);
-    image3.inputEnabled = true;
+
+
+	var image3 = game.add.sprite(750, 400, 'carrot2');
+	image3.scale.setTo(0.5, 0.5);
+	image3.inputEnabled = true;
 
 	image3.events.onInputDown.add(fail, this);
 	image3.events.onInputDown.add(action1, this);
 	image3.events.onInputDown.add(animation4, this);
 
 
-	
-	
-	var image4 = game.add.sprite(950, 400, 'carot1');
-    image4.scale.setTo(0.5,0.5);
-    image4.inputEnabled = true;
+
+
+	var image4 = game.add.sprite(950, 400, 'carrot1');
+	image4.scale.setTo(0.5, 0.5);
+	image4.inputEnabled = true;
 
 	image4.events.onInputDown.add(score2, this);
 	image4.events.onInputDown.add(action, this);
 	image4.events.onInputDown.add(animation2, this);
 	image4.events.onInputDown.add(process1, this);
-	
-	var gio = game.add.sprite(20, 420, 'gio');
-	gio.scale.setTo(0.3, 0.3);
-	
-	
-	//var btn = game.add.button(900, 500, 'btn', process, this);
-	//btn.scale.setTo(0.3);
+
+	var basket = game.add.sprite(20, 420, 'basket');
+	basket.scale.setTo(0.3, 0.3);
+
+
+	//var btnNext = game.add.button(900, 500, 'btnNext', process, this);
+	//btnNext.scale.setTo(0.3);
 
 
 }
@@ -107,129 +99,129 @@ function create() {
 
 function start() {
 
-    music6.fadeIn(4000);
+	music6.fadeIn(4000);
 
 }
 
 
 
-function score(){
-		
-		music = game.add.audio('music');
-		music.play();
-		
-		music = game.add.audio('music5');
-		music.play();
-		
-		
-}
+function score() {
 
-function score1(){
-		music = game.add.audio('music');
-		music.play();
+	music = game.add.audio('music');
+	music.play();
 
-		
-		music = game.add.audio('music5');
-		music.play();
+	music = game.add.audio('music5');
+	music.play();
+
 
 }
 
-function score2(){
-		music = game.add.audio('music');
-		music.play();
+function score1() {
+	music = game.add.audio('music');
+	music.play();
 
-		
-		music = game.add.audio('music5');
-		music.play();
 
-}
-
-function fail(){
-		music = game.add.audio('music1');
-		music.play();
+	music = game.add.audio('music5');
+	music.play();
 
 }
 
-function action(image){
+function score2() {
+	music = game.add.audio('music');
+	music.play();
+
+
+	music = game.add.audio('music5');
+	music.play();
+
+}
+
+function fail() {
+	music = game.add.audio('music1');
+	music.play();
+
+}
+
+function action(image) {
 	image.visible = image.scale.setTo(0.4);
 
 }
-function action1(image1){
-	
+function action1(image1) {
+
 	image1.visible = image1.scale.setTo(0.4);
 }
 
-function action(image2){
+function action(image2) {
 
 	image2.visible = image2.scale.setTo(0.4);
 }
-function action1(image3){
-	
+function action1(image3) {
+
 	image3.visible = image3.scale.setTo(0.4);
 }
 
-function action(image4){
+function action(image4) {
 
 	image4.visible = image4.scale.setTo(0.4);
 }
 
-function animation(image){
+function animation(image) {
 	image.inputEnabled = false;
-	diem ++;
+	diem++;
 	//scoretext.text = diem;
 	image.animations.add('teeter');
-    image.animations.play('teeter', 5, true);
-    game.add.tween(image).to({ y: [100, 400], x: [200, 50] }, 2000, Phaser.Easing.Quadratic.InOut, true);
-	
+	image.animations.play('teeter', 5, true);
+	game.add.tween(image).to({ y: [100, 400], x: [200, 50] }, 2000, Phaser.Easing.Quadratic.InOut, true);
+
 }
 
-function animation1(image2){
+function animation1(image2) {
 	image2.inputEnabled = false;
-	diem ++;
+	diem++;
 	//scoretext.text = diem;
 	image2.animations.add('teeter');
-    image2.animations.play('teeter', 5, true);
-    game.add.tween(image2).to({ y: [100, 420], x: [200, 70] }, 2000, Phaser.Easing.Quadratic.InOut, true);
+	image2.animations.play('teeter', 5, true);
+	game.add.tween(image2).to({ y: [100, 420], x: [200, 70] }, 2000, Phaser.Easing.Quadratic.InOut, true);
 }
 
-function animation2(image4){
+function animation2(image4) {
 	image4.inputEnabled = false;
-	diem ++;
+	diem++;
 	//scoretext.text = diem;
 	image4.animations.add('teeter');
-    image4.animations.play('teeter', 5, true);
-    game.add.tween(image4).to({ y: [100, 440], x: [200, 90] }, 2000, Phaser.Easing.Quadratic.InOut, true);
+	image4.animations.play('teeter', 5, true);
+	game.add.tween(image4).to({ y: [100, 440], x: [200, 90] }, 2000, Phaser.Easing.Quadratic.InOut, true);
 }
 
-function animation3(image1){
+function animation3(image1) {
 	image1.animations.add('teeter');
-    image1.animations.play('teeter', 5, true);
-   	game.add.tween(image1).to({ y: [250, 450], x: [350, 350] }, 1000, Phaser.Easing.Quadratic.InOut, true);
+	image1.animations.play('teeter', 5, true);
+	game.add.tween(image1).to({ y: [250, 450], x: [350, 350] }, 1000, Phaser.Easing.Quadratic.InOut, true);
 
 }
 
-function animation4(image3){
+function animation4(image3) {
 	image3.animations.add('teeter');
-    image3.animations.play('teeter', 5, true);
-    game.add.tween(image3).to({ y: [250, 450], x: [750, 750] }, 1000, Phaser.Easing.Quadratic.InOut, true);
+	image3.animations.play('teeter', 5, true);
+	game.add.tween(image3).to({ y: [250, 450], x: [750, 750] }, 1000, Phaser.Easing.Quadratic.InOut, true);
 }
 
-function process1(){
-	if( diem >= 3 ){
+function process1() {
+	if (diem >= 3) {
 		music = game.add.audio('music3');
 		music.play();
 		game.time.events.add(Phaser.Timer.SECOND * 3, Delay1, this);
 	}
-	else{
+	else {
 		false;
 	}
 }
 
-function Delay1(){
-	var winx = window.location="/bcm/level1/game2/index.html";
+function Delay1() {
+	var winx = window.location = "/api/unit1_game2";
 }
 
-function update(){
-	
+function update() {
+
 }
 
--- public/js/unit2_1.js
+++ public/js/unit2_1.js
@@ -1,257 +1,255 @@
-﻿var game = new Phaser.Game(1366,768,Phaser.CANVAS, 'gameContainer',{ preload: preload, create: create, update:update,render:render });
+﻿var game = new Phaser.Game(1366, 768, Phaser.CANVAS, 'gameContainer', { preload: preload, create: create, update: update, render: render });
 
-	// Creates a new 'main' state that wil contain the game
-	var background;
-	var basket;
-	var flower = new Array(5);
-	var sound = new Array(4);
-	var tween = new Array(5);
-	var score=0;
-	var button,button1;
-	var grass;
-	var window_next ;
-	//var sound =new Array();
-	// Function called first to load all the assetsme
-	function preload(){
+// Creates a new 'main' state that wil contain the game
+var background;
+var basket;
+var flower = new Array(5);
+var sound = new Array(4);
+var tween = new Array(5);
+var score = 0;
+var button, button1;
+var grass;
+var window_next;
+//var sound =new Array();
+// Function called first to load all the assetsme
+function preload() {
 	// load background
-	
-		game.load.image('bg',BACKGROUND_IMAGE);
-		
+
+	game.load.image('bg', BACKGROUND_IMAGE);
+
 	//load basket
 
-		game.load.image('bk',BASKET_IMAGE);
+	game.load.image('bk', BASKET_IMAGE);
 
 	// load flower 
 
-		game.load.image('flower1',FLOWER_1);
-		game.load.image('flower2',FLOWER_2);
-		/*game.load.image('flower3',FLOWER_3);*/
+	game.load.image('flower1', FLOWER_1);
+	game.load.image('flower2', FLOWER_2);
+	/*game.load.image('flower3',FLOWER_3);*/
 
 	// load sound
 
-		game.load.audio('start',SOUND_START);
-		game.load.audio('score',SOUND_SCORE);
-		game.load.audio('fail',SOUND_FAIL);
-		game.load.audio('end',SOUND_END);
+	game.load.audio('start', SOUND_START);
+	game.load.audio('score', SOUND_SCORE);
+	game.load.audio('fail', SOUND_FAIL);
+	game.load.audio('end', SOUND_END);
 	// load button
 
-		/*game.load.image('btn','assets/images/nextbutton.png');*/
-		game.load.image('again',BUTTON_AGAIN);
-		game.load.image('btn',BUTTON_IMAGE);
-		game.load.image('next',BUTTON_NEXT);
-	
-	}
-	
+	/*game.load.image('btn','assets/images/nextbutton.png');*/
+	game.load.image('again', BUTTON_AGAIN);
+	game.load.image('btn', BUTTON_IMAGE);
+	game.load.image('next', BUTTON_NEXT);
 
-	// Fuction called after 'preload' to setup the game  
-	function create(){
+}
+
+
+// Fuction called after 'preload' to setup the game  
+function create() {
 
 	// create background
 
-		//background = game.add.sprite(BACKGROUND_POSX,BACKGROUND_POSY,'bg');
-		background = game.add.sprite(0,0,'bg');
-		/*background.anchor.setTo(0.01);*/
-		/*background.scale.setTo(2);*/
-		
+	//background = game.add.sprite(BACKGROUND_POSX,BACKGROUND_POSY,'bg');
+	background = game.add.sprite(0, 0, 'bg');
+	/*background.anchor.setTo(0.01);*/
+	/*background.scale.setTo(2);*/
+
 	// create sprite flowers
-		flower[0] = game.add.sprite(FLOWER_1_POSX,FLOWER_1_POSY,'flower1');
-		flower[0].scale.setTo(0.5,0.8);
-		
-		flower[1] = game.add.sprite(FLOWER_2_POSX,FLOWER_2_POSY,'flower2');
-		flower[1].scale.setTo(0.5,0.8);
+	flower[0] = game.add.sprite(FLOWER_1_POSX, FLOWER_1_POSY, 'flower1');
+	flower[0].scale.setTo(0.5, 0.8);
 
-		flower[2] = game.add.sprite(FLOWER_3_POSX,FLOWER_3_POSY,'flower2');
-		flower[2].scale.setTo(0.5,0.8);
+	flower[1] = game.add.sprite(FLOWER_2_POSX, FLOWER_2_POSY, 'flower2');
+	flower[1].scale.setTo(0.5, 0.8);
 
-		flower[3] = game.add.sprite(FLOWER_4_POSX,FLOWER_4_POSY,'flower1');
-		flower[3].scale.setTo(0.5,0.8);
+	flower[2] = game.add.sprite(FLOWER_3_POSX, FLOWER_3_POSY, 'flower2');
+	flower[2].scale.setTo(0.5, 0.8);
 
-		flower[4] = game.add.sprite(FLOWER_5_POSX,FLOWER_5_POSY,'flower2');
-		flower[4].scale.setTo(0.5,0.8);		
+	flower[3] = game.add.sprite(FLOWER_4_POSX, FLOWER_4_POSY, 'flower1');
+	flower[3].scale.setTo(0.5, 0.8);
+
+	flower[4] = game.add.sprite(FLOWER_5_POSX, FLOWER_5_POSY, 'flower2');
+	flower[4].scale.setTo(0.5, 0.8);
 
 	// add sound
-		sound[0] = game.add.audio('start');
-		sound[0].play();
-		sound[1] =game.add.audio('score');
-		sound[2] =game.add.audio('fail');
-		sound[3] =game.add.audio('end');
+	sound[0] = game.add.audio('start');
+	sound[0].play();
+	sound[1] = game.add.audio('score');
+	sound[2] = game.add.audio('fail');
+	sound[3] = game.add.audio('end');
 	// add tween
-		tween[0] = game.add.tween(flower[0]);
-		tween[1] =game.add.tween(flower[1]);
-		tween[2] = game.add.tween(flower[2]);
-		tween[3] =game.add.tween(flower[3]);
-		tween[4] =game.add.tween(flower[4]);
-	
+	tween[0] = game.add.tween(flower[0]);
+	tween[1] = game.add.tween(flower[1]);
+	tween[2] = game.add.tween(flower[2]);
+	tween[3] = game.add.tween(flower[3]);
+	tween[4] = game.add.tween(flower[4]);
+
 	// process click
-		flower[0].inputEnabled = true;
-		flower[0].events.onInputDown.add(listenerWrong1, this);
-		
-		flower[1].inputEnabled = true;
-		flower[1].events.onInputDown.add(listenerRight1, this);
-	
+	flower[0].inputEnabled = true;
+	flower[0].events.onInputDown.add(listenerWrong1, this);
 
-		flower[2].inputEnabled = true;
-		flower[2].events.onInputDown.add(listenerRight2, this);
-	
-		
-		flower[3].inputEnabled = true;
-		flower[3].events.onInputDown.add(listenerWrong2,this);
-		
+	flower[1].inputEnabled = true;
+	flower[1].events.onInputDown.add(listenerRight1, this);
 
-		flower[4].inputEnabled = true;
-		flower[4].events.onInputDown.add(listenerRight3, this);
-		
+
+	flower[2].inputEnabled = true;
+	flower[2].events.onInputDown.add(listenerRight2, this);
+
+
+	flower[3].inputEnabled = true;
+	flower[3].events.onInputDown.add(listenerWrong2, this);
+
+
+	flower[4].inputEnabled = true;
+	flower[4].events.onInputDown.add(listenerRight3, this);
+
 	// create basket
 
-      	basket = game.add.sprite(8,350,'bk');
-      	basket.scale.setTo(0.4);
-    // add button
-    	button = game.add.button(900, 600, 'btn',Process, this);
+	basket = game.add.sprite(8, 350, 'bk');
+	basket.scale.setTo(0.4);
+	// add button
+	button = game.add.button(900, 600, 'btn', Process, this);
+	button.scale.setTo(0.4);
+
+	/*button = game.add.button(500, 570, 'btn',process,this, 2, 1, 0);
+button.scale.setTo(0.4);*/
+	/*process();*/
+
+}
+/*function openlink_level2_game2(){
+	window.location="http://112.78.11.147/bcm/level2/game2";
+}*/
+
+function listenerRight1() {
+
+	sound[1].play();
+	flower[1].scale.setTo(0.4);
+	tween[1].to({ y: 340, x: 70 }, 1500, Phaser.Easing.Quadratic.InOut, true);
+	skew1();
+	score++;
+	sound[3].play();
+	flower[1].inputEnabled = false;
+}
+function listenerRight2() {
+
+	sound[1].play();
+	flower[2].scale.setTo(0.4);
+	tween[2].to({ y: 350, x: 150 }, 1500, Phaser.Easing.Quadratic.InOut, true);
+	skew2();
+	score++;
+	sound[3].play();
+	flower[2].inputEnabled = false;
+}
+function listenerRight3() {
+
+	sound[1].play();
+	flower[4].scale.setTo(0.4);
+	tween[4].to({ y: 380, x: 120 }, 1500, Phaser.Easing.Quadratic.InOut, true);
+	skew3();
+	score++;
+	sound[3].play();
+	flower[4].inputEnabled = false;
+
+}
+function skew1() {
+
+	flower[1].anchor.setTo(0);
+	flower[1].angle = 30;
+}
+function skew2() {
+
+	flower[2].anchor.setTo(0);
+	flower[2].angle = 35;
+}
+
+function skew3() {
+
+	flower[4].anchor.setTo(0);
+	flower[4].angle = 40;
+}
+
+function listenerWrong1() {
+
+	sound[2].play();
+	var count = 0;
+	/*flower[0].anchor.x=-0.1;
+	flower[0].anchor.y=-0.00005;*/
+	while (count < 10) {
+		if (count % 2 == 0) {
+			tween[0].to({ angle: 2 }, 50).start();
+		} else {
+			tween[0].to({ angle: -2 }, 50).start();
+		}
+		count++;
+	}
+	tween[0].to({ angle: 0 }, 50).start();
+	flower[0].inputEnabled = false;
+
+}
+function listenerWrong2() {
+
+	sound[2].play();
+	var count = 0;
+	while (count < 10) {
+		if (count % 2 == 0) {
+			tween[3].to({ angle: 1 }, 50).start();
+		} else {
+			tween[3].to({ angle: -1 }, 50).start()
+		}
+		count++;
+	}
+	tween[3].to({ angle: 0 }, 50).start();
+	flower[3].inputEnabled = false;
+
+}
+function Process() {
+
+	button.visible = false;
+	if (score == 3) {
+		button = game.add.button(900, 600, 'next', Next, this, 2, 1, 0);
 		button.scale.setTo(0.4);
-
-   		/*button = game.add.button(500, 570, 'btn',process,this, 2, 1, 0);
-		button.scale.setTo(0.4);*/
-		/*process();*/
-		
-    }
-    /*function openlink_level2_game2(){
-    	window.location="http://112.78.11.147/bcm/level2/game2";
-    }*/
-   
-    function listenerRight1(){
-
-    	sound[1].play();
-    	flower[1].scale.setTo(0.4);
-    	tween[1].to({y: 340, x:70},1500,Phaser.Easing.Quadratic.InOut,true);
-    	skew1();
-    	score++;
-    	sound[3].play();
-    	flower[1].inputEnabled = false;
-    }
-    function listenerRight2(){
-
-    	sound[1].play();
-    	flower[2].scale.setTo(0.4);
-    	tween[2].to({y:350, x:150},1500,Phaser.Easing.Quadratic.InOut,true);
-    	skew2();
-    	score++;
-    	sound[3].play();
-    	flower[2].inputEnabled = false;
-    }
-    function listenerRight3(){
-
-    	sound[1].play();
-    	flower[4].scale.setTo(0.4);
-    	tween[4].to({y:380, x:120},1500,Phaser.Easing.Quadratic.InOut,true);
-    	skew3();
-    	score++;
-    	sound[3].play();
-    	flower[4].inputEnabled = false;
-    	
-    }
-     function skew1(){
-
-    	flower[1].anchor.setTo(0);
-    	flower[1].angle=30;
-    }
-    function skew2(){
-
-    	flower[2].anchor.setTo(0);
-    	flower[2].angle=35;
-    }
-   
-    function skew3(){
-
-    	flower[4].anchor.setTo(0);
-    	flower[4].angle=40;
-    }
-	
-    function listenerWrong1(){
-
-    	sound[2].play();
-    	var count =0;
-    	/*flower[0].anchor.x=-0.1;
-    	flower[0].anchor.y=-0.00005;*/
-    	while(count<10){
-    		if(count%2==0){
-    			tween[0].to({angle:2},50).start();
-    		}else{
-    			tween[0].to({angle:-2},50).start();
-    		}
-    		count++;
-    	}
-    	tween[0].to({angle:0},50).start();
-    	flower[0].inputEnabled = false;
-
-    }
-    function listenerWrong2(){
-
-    	sound[2].play();
-    	var count =0;
-    	while(count<10){
-    		if(count%2==0){
-    			tween[3].to({angle:1},50).start();
-    		}else{
-    			tween[3].to({angle:-1},50).start()
-    		}
-    		count++;
-    	}
-    	tween[3].to({angle:0},50).start();
-    	flower[3].inputEnabled = false;
-    	
-    }
-    function Process(){
-
-    	button.visible = false;
-	 	if (score ==3)
-		{   
-			button = game.add.button(900, 600, 'next',Next,this, 2, 1, 0);
-			button.scale.setTo(0.4);
-			sound[1].play();
-			game.time.events.add(Phaser.Timer.SECOND * 1, Delay, this);			
-			/*var String_1 ='Hoan hô, bạn giỏi lắm'
-			var info = game.add.text(300, 40,String_1,style);*/
-    	}else{
-    		/*var String_2 ='Bạn ơi, thiếu rùi \n mình làm lại nhé'
-			var info = game.add.text(300, 40,String_2,style);*/
-			sound[2].play();
-			Button();
-    	}
-    }
-	function Next(){
-		
-		window_next = window.location="api/unit1_game2";
-		
+		sound[1].play();
+		game.time.events.add(Phaser.Timer.SECOND * 1, Delay, this);
+		/*var String_1 ='Hoan hô, bạn giỏi lắm'
+		var info = game.add.text(300, 40,String_1,style);*/
+	} else {
+		/*var String_2 ='Bạn ơi, thiếu rùi \n mình làm lại nhé'
+		var info = game.add.text(300, 40,String_2,style);*/
+		sound[2].play();
+		Button();
 	}
-	function Delay(){
+}
+function Next() {
 
-		game.add.tween(sound[3].play()).to( 2000, Phaser.Easing.Linear.None, true);		
-	}
-	function Button(){	
+	window_next = window.location = "api/unit1_game2";
 
-		flower[0].inputEnabled = false;
-		flower[1].inputEnabled = false;
-		flower[2].inputEnabled = false;
-		flower[3].inputEnabled = false;
-		flower[4].inputEnabled = false;
-		button1 = game.add.button(900, 600, 'again',ProcessAgain,this);
-		button1.scale.setTo(0.4);
-	}
-	function ProcessAgain()
-	{
-		game.state.start(game.state.current);
-		//game.state.restart();
-		//game.state.start(game.state.current);
-		//var winx = window.location="index.html";
-	}
-	// Function called 60 times per second
-	function update(){
-		game.scale.forceOrientation(false, true);
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		//this.scale.pageAlignHorizontally = true;
-		//this.scale.pageAlignVertically = true;
-		//this.scale.setScreenSize( true );
-	}
-	function render(){
-		/*game.debug.inputInfo(32, 32);*/
-	}
+}
+function Delay() {
+
+	game.add.tween(sound[3].play()).to(2000, Phaser.Easing.Linear.None, true);
+}
+function Button() {
+
+	flower[0].inputEnabled = false;
+	flower[1].inputEnabled = false;
+	flower[2].inputEnabled = false;
+	flower[3].inputEnabled = false;
+	flower[4].inputEnabled = false;
+	button1 = game.add.button(900, 600, 'again', ProcessAgain, this);
+	button1.scale.setTo(0.4);
+}
+function ProcessAgain() {
+	game.state.start(game.state.current);
+	//game.state.restart();
+	//game.state.start(game.state.current);
+	//var winx = window.location="index.html";
+}
+// Function called 60 times per second
+function update() {
+	game.scale.forceOrientation(false, true);
+	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	//this.scale.pageAlignHorizontally = true;
+	//this.scale.pageAlignVertically = true;
+	//this.scale.setScreenSize( true );
+}
+function render() {
+	/*game.debug.inputInfo(32, 32);*/
+}
