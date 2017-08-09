faf32aac8842c817af44065623d59203d04a48d3
PC_BSS <hobinhit@gmail.com>
2017-08-09 21:04:40 +0700

    finish game 7.2

--- public/js/object7_2.js
+++ public/js/object7_2.js
@@ -9,62 +9,62 @@ var style={font:"70px Arial", fill:"#FFFFFF"}
 
 //BACKGROUND
 
-const BACKGROUND_IMAGE = 'assets/images/unit7_game1/bg.png';
+const BACKGROUND_IMAGE = 'assets/images/unit7_game2/bg.png';
 const BACKGROUND_POSX = 0;
 const BACKGROUND_POSY = 0;
-const BACKGROUND_WIDTH = 1024;
-const BACKGROUND_HEIGHT = 720;
+const BACKGROUND_WIDTH = 1366;
+const BACKGROUND_HEIGHT = 768;
 
 
 
 //DUCK
-const DUCK_1 ="assets/images/unit7_game1/duck_1.png";
-const DUCK_2 ="assets/images/unit7_game1/duck_2.png";
-const DUCK_3 ="assets/images/unit7_game1/duck_3.png";
-const DUCK_4 ="assets/images/unit7_game1/duck_4.png";
-const DUCK_5 ="assets/images/unit7_game1/duck_5.png";
-const DUCK_6 ="assets/images/unit7_game1/duck_6.png";
-const DUCK_7 ="assets/images/unit7_game1/duck_7.png";
+const FROG_1 ="assets/images/unit7_game2/frog_1.png";
+const FROG_2 ="assets/images/unit7_game2/frog_2.png";
+const FROG_3 ="assets/images/unit7_game2/frog_3.png";
+const FROG_4 ="assets/images/unit7_game2/frog_4.png";
+const FROG_5 ="assets/images/unit7_game2/frog_5.png";
+const FROG_6 ="assets/images/unit7_game2/frog_6.png";
+const FROG_7 ="assets/images/unit7_game2/frog_7.png";
 
 
 
 //Position
 
 
-const DUCK_1_POSX = 540;
-const DUCK_1_POSY = 220;
+const FROG_1_POSX = 800;
+const FROG_1_POSY = 20;
 
-const DUCK_2_POSX = 150;
-const DUCK_2_POSY = 190;
+const FROG_2_POSX = 200;
+const FROG_2_POSY = -10;
 
-const DUCK_3_POSX = 670;
-const DUCK_3_POSY = 220;
+const FROG_3_POSX = 1000;
+const FROG_3_POSY = 20;
 
-const DUCK_4_POSX = 410;
-const DUCK_4_POSY = 200;
+const FROG_4_POSX = 600;
+const FROG_4_POSY = -10;
 
-const DUCK_5_POSX = 20;
-const DUCK_5_POSY = 220;
+const FROG_5_POSX = 20;
+const FROG_5_POSY = 20;
 
-const DUCK_6_POSX = 280;
-const DUCK_6_POSY = 220;
+const FROG_6_POSX = 400;
+const FROG_6_POSY = 20;
 
-const DUCK_7_POSX = 800;
-const DUCK_7_POSY = 190;
+const FROG_7_POSX = 1200;
+const FROG_7_POSY = -10;
 
 
 
 // button 
-const BUTTON_IMAGE="assets/images/unit7_game1/ic_button_back.png";
-const BUTTON_AGAIN="assets/images/unit7_game1/ic_button_restart.png";
-const BUTTON_NEXT="assets/images/unit7_game1/ic_button_next.png";
+const BUTTON_IMAGE="assets/images/unit7_game2/ic_button_back.png";
+const BUTTON_AGAIN="assets/images/unit7_game2/ic_button_restart.png";
+const BUTTON_NEXT="assets/images/unit7_game2/ic_button_next.png";
 
 
 
 // Load sound
-const SOUND_START ="assets/sounds/unit7_1/start.mp3";
-const SOUND_DROP ="assets/sounds/unit7_1/drop.mp3";
-const SOUND_SCORE ="assets/sounds/unit7_1/Score.mp3";
-const SOUND_FAIL ="assets/sounds/unit7_1/Fail.mp3";
-const SOUND_END ="assets/sounds/unit7_1/End.mp3";
+const SOUND_START ="assets/sounds/unit7_2/start.mp3";
+const SOUND_DROP ="assets/sounds/unit7_2/drop.mp3";
+const SOUND_SCORE ="assets/sounds/unit7_2/Score.mp3";
+const SOUND_FAIL ="assets/sounds/unit7_2/Fail.mp3";
+const SOUND_END ="assets/sounds/unit7_2/End.mp3";
 
--- public/js/unit7_2.js
+++ public/js/unit7_2.js
@@ -4,7 +4,7 @@ var game = new Phaser.Game(1366, 768, Phaser.CANVAS, 'gameContainer', { preload:
 var background;
 var basket;
 var text;
-var Basket_1, Basket_2, Basket_3, Basket_4;
+var Frog_1, Frog_2, Frog_3, Frog_4;
 var sound = new Array(4);
 var ScoreText;
 var Score = 0;
@@ -22,16 +22,14 @@ function preload() {
 	game.load.image('bg', BACKGROUND_IMAGE);
 	//game.scale.forceOrientation(false, true);
 
-	//load Spuirrel
-	game.load.image('Spuirrel1', SPUIRREL_IMAGE1);
-	game.load.image('Spuirrel2', SPUIRREL_IMAGE2);
-	game.load.image('Spuirrel3', SPUIRREL_IMAGE3);
-	game.load.image('Spuirrel4', SPUIRREL_IMAGE4);
 	// load Basket
-	game.load.image('Basket_1', BASKET_1);
-	game.load.image('Basket_2', BASKET_2);
-	game.load.image('Basket_3', BASKET_3);
-	game.load.image('Basket_4', BASKET_4);
+	game.load.spritesheet('Frog_1', FROG_1, 140, 236, 8);
+	game.load.spritesheet('Frog_2', FROG_2, 140, 236, 8);
+	game.load.spritesheet('Frog_3', FROG_3, 140, 236, 8);
+	game.load.spritesheet('Frog_4', FROG_4, 140, 236, 8);
+	game.load.spritesheet('Frog_5', FROG_5, 140, 236, 8);
+	game.load.spritesheet('Frog_6', FROG_6, 140, 236, 8);
+	game.load.spritesheet('Frog_7', FROG_7, 140, 236, 8);
 
 	// load sound
 	game.load.audio('start', SOUND_START);
@@ -40,8 +38,6 @@ function preload() {
 	//game.load.audio('end',SOUND_END);
 	game.load.audio('drop', SOUND_DROP);
 	// load button
-	//game.load.image('again',BUTTON_AGAIN);
-	//game.load.image('btn',BUTTON_IMAGE);
 	game.load.image('next', BUTTON_NEXT);
 	// process responsive
 
@@ -65,42 +61,67 @@ function create() {
 	sound[3] = game.add.audio('end');
 	sound[4] = game.add.audio('drop');
 
-	// create Spuirrel
-	Spuirrel_1 = game.add.sprite(40, 450, 'Spuirrel1');
-	Spuirrel_1.scale.setTo(0.7);
-	Spuirrel_2 = game.add.sprite(396, 440, 'Spuirrel2');
-	Spuirrel_2.scale.setTo(0.7);
-	Spuirrel_3 = game.add.sprite(754, 450, 'Spuirrel3');
-	Spuirrel_3.scale.setTo(0.7);
-	Spuirrel_4 = game.add.sprite(1099, 440, 'Spuirrel4');
-	Spuirrel_4.scale.setTo(0.7);
+	Frog_1 = game.add.sprite(FROG_1_POSX, FROG_1_POSY, 'Frog_1');
+	Frog_1.scale.setTo(0.9);
+	Frog_2 = game.add.sprite(FROG_2_POSX, FROG_2_POSY, 'Frog_2');
+	Frog_2.scale.setTo(0.9);
+	Frog_3 = game.add.sprite(FROG_3_POSX, FROG_3_POSY, 'Frog_3');
+	Frog_3.scale.setTo(0.9);
+	Frog_4 = game.add.sprite(FROG_4_POSX, FROG_4_POSY, 'Frog_4');
+	Frog_4.scale.setTo(0.9);
+	Frog_5 = game.add.sprite(FROG_5_POSX, FROG_5_POSY, 'Frog_5');
+	Frog_5.scale.setTo(0.9);
+	Frog_6 = game.add.sprite(FROG_6_POSX, FROG_6_POSY, 'Frog_6');
+	Frog_6.scale.setTo(0.9);
+	Frog_7 = game.add.sprite(FROG_7_POSX, FROG_7_POSY, 'Frog_7');
+	Frog_7.scale.setTo(0.9);
 
 
-	Basket_1 = game.add.sprite(BASKET_1_POSX, BASKET_1_POSY, 'Basket_1');
-	Basket_1.scale.setTo(0.7);
-	Basket_2 = game.add.sprite(BASKET_2_POSX, BASKET_2_POSY, 'Basket_2');
-	Basket_2.scale.setTo(0.7);
-	Basket_3 = game.add.sprite(BASKET_3_POSX, BASKET_3_POSY, 'Basket_3');
-	Basket_3.scale.setTo(0.7);
-	Basket_4 = game.add.sprite(BASKET_4_POSX, BASKET_4_POSY, 'Basket_4');
-	Basket_4.scale.setTo(0.7);
-	//create drag and drop Basket		
-	Basket_1.inputEnabled = true;					//cho phep nhap						
-	Basket_1.input.enableDrag();                     //kich hoat keo tha
-	Basket_1.events.onInputDown.add(score_s, this);	//âm thanh khi kéo
-	Basket_1.events.onDragStop.add(stopDrag_1);		// quy định các luật dừng
-	Basket_2.inputEnabled = true;
-	Basket_2.input.enableDrag();
-	Basket_2.events.onInputDown.add(score_s, this);
-	Basket_2.events.onDragStop.add(stopDrag_2);
-	Basket_3.inputEnabled = true;
-	Basket_3.input.enableDrag();
-	Basket_3.events.onInputDown.add(score_s, this);
-	Basket_3.events.onDragStop.add(stopDrag_3);
-	Basket_4.inputEnabled = true;
-	Basket_4.input.enableDrag();
-	Basket_4.events.onInputDown.add(score_s, this);
-	Basket_4.events.onDragStop.add(stopDrag_4);
+
+	Frog_1.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+	Frog_2.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+	Frog_3.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+	Frog_4.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+	Frog_5.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+	Frog_6.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+	Frog_7.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8], 8, true);
+
+	Frog_1.animations.play('walk');
+	Frog_2.animations.play('walk');
+	Frog_3.animations.play('walk');
+	Frog_4.animations.play('walk');
+	Frog_5.animations.play('walk');
+	Frog_6.animations.play('walk');
+	Frog_7.animations.play('walk');
+
+	Frog_1.inputEnabled = true;					//cho phep nhap						
+	Frog_1.input.enableDrag();                     //kich hoat keo tha
+	Frog_1.events.onInputDown.add(score_s, this);	//âm thanh khi kéo
+	Frog_1.events.onDragStop.add(stopDrag_1);		// quy định các luật dừng
+	Frog_2.inputEnabled = true;
+	Frog_2.input.enableDrag();
+	Frog_2.events.onInputDown.add(score_s, this);
+	Frog_2.events.onDragStop.add(stopDrag_2);
+	Frog_3.inputEnabled = true;
+	Frog_3.input.enableDrag();
+	Frog_3.events.onInputDown.add(score_s, this);
+	Frog_3.events.onDragStop.add(stopDrag_3);
+	Frog_4.inputEnabled = true;
+	Frog_4.input.enableDrag();
+	Frog_4.events.onInputDown.add(score_s, this);
+	Frog_4.events.onDragStop.add(stopDrag_4);
+	Frog_5.inputEnabled = true;
+	Frog_5.input.enableDrag();
+	Frog_5.events.onInputDown.add(score_s, this);
+	Frog_5.events.onDragStop.add(stopDrag_5);
+	Frog_6.inputEnabled = true;
+	Frog_6.input.enableDrag();
+	Frog_6.events.onInputDown.add(score_s, this);
+	Frog_6.events.onDragStop.add(stopDrag_6);
+	Frog_7.inputEnabled = true;
+	Frog_7.input.enableDrag();
+	Frog_7.events.onInputDown.add(score_s, this);
+	Frog_7.events.onDragStop.add(stopDrag_7);
 
 }
 
@@ -110,102 +131,153 @@ function score_s() {
 
 //drap and drop Basket 1
 function stopDrag_1() {
-	Basket_1.scale.setTo(0.3);
-	if (Basket_1.y > 414 && Basket_1.y < 564 && Basket_1.x > 736 && Basket_1.x < 866) {
-		Basket_1.inputEnabled = false;
-		Basket_1.x = '730.5';
-		Basket_1.y = '556.1';
+	Frog_1.scale.setTo(0.7);
+	if (Frog_1.y > 264.8 && Frog_1.y < 364.7 && Frog_1.x > -57.8 && Frog_1.x < 200) {
+		Frog_1.inputEnabled = false;
+		Frog_1.x = '84';
+		Frog_1.y = '346';
 		Score++;
 		sound[1].play();
 	} else {
-		Basket_1.x = BASKET_1_POSX;
-		Basket_1.y = BASKET_1_POSY;
-		Basket_1.scale.setTo(0.7);
+		Frog_1.x = FROG_1_POSX;
+		Frog_1.y = FROG_1_POSY;
+		Frog_1.scale.setTo(0.9);
 		sound[2].play();
 	}
 	Process();
 }
 //drag and drop Basket2
 function stopDrag_2() {
-	Basket_2.scale.setTo(0.3);
-	if (Basket_2.y > 414 && Basket_2.y < 564 && Basket_2.x > 360 && Basket_2.x < 500) {
-		Basket_2.inputEnabled = false;
-		Basket_2.x = '396';
-		Basket_2.y = '545.9';
+	Frog_2.scale.setTo(0.7);
+	if (Frog_2.y > 130 && Frog_2.y < 223 && Frog_2.x > 786 && Frog_2.x < 1009) {
+		Frog_2.inputEnabled = false;
+		Frog_2.x = '910';
+		Frog_2.y = '204';
 		Score++;
 		sound[1].play();
 	} else {
 
-		Basket_2.x = BASKET_2_POSX;
-		Basket_2.y = BASKET_2_POSY;
-		Basket_2.scale.setTo(0.7);
+		Frog_2.x = FROG_2_POSX;
+		Frog_2.y = FROG_2_POSY;
+		Frog_2.scale.setTo(0.9);
 		sound[2].play();
 	}
 	Process();
 }
 //drag and drop Basket3
 function stopDrag_3() {
-	Basket_3.scale.setTo(0.3);
-	if (Basket_3.y > 414 && Basket_3.y < 564 && Basket_3.x > 1060 && Basket_3.x < 1200) {
-		Basket_3.inputEnabled = false;
-		Basket_3.x = '1096.2';
-		Basket_3.y = '545.9';
+	Frog_3.scale.setTo(0.7);
+	if (Frog_3.y > 447 && Frog_3.y < 606 && Frog_3.x > 325 && Frog_3.x < 681) {
+		Frog_3.inputEnabled = false;
+		Frog_3.x = '511';
+		Frog_3.y = '562';
 		Score++;
 		sound[1].play();
 	} else {
-		Basket_3.x = BASKET_3_POSX;
-		Basket_3.y = BASKET_3_POSY;
-		Basket_3.scale.setTo(0.7);
+		Frog_3.x = FROG_3_POSX;
+		Frog_3.y = FROG_3_POSY;
+		Frog_3.scale.setTo(0.9);
 		sound[2].play();
 	}
 	Process();
 }
 //drag and drop Basket4
 function stopDrag_4() {
-	Basket_4.scale.setTo(0.3);
-	if (Basket_4.y > 414 && Basket_4.y < 564 && Basket_4.x > 14 && Basket_4.x < 142) {
-		Basket_4.inputEnabled = false;
-		Basket_4.x = '35.3';
-		Basket_4.y = '557.1';
+	Frog_4.scale.setTo(0.7);
+	if (Frog_4.y > 116 && Frog_4.y < 229 && Frog_4.x > 57.8 && Frog_4.x < 172) {
+		Frog_4.inputEnabled = false;
+		Frog_4.x = '46.7';
+		Frog_4.y = '197.5';
 		Score++;
 		sound[1].play();
 	} else {
-		Basket_4.x = BASKET_4_POSX;
-		Basket_4.y = BASKET_4_POSY;
-		Basket_4.scale.setTo(0.7);
+		Frog_4.x = FROG_4_POSX;
+		Frog_4.y = FROG_4_POSY;
+		Frog_4.scale.setTo(0.9);
 		sound[2].play();
 	}
 	Process();
 }
 
+function stopDrag_5() {
+	Frog_5.scale.setTo(0.7);
+	if (Frog_5.y > 355 && Frog_5.y < 448 && Frog_5.x > 840 && Frog_5.x < 1095) {
+		Frog_5.inputEnabled = false;
+		Frog_5.x = '988.6';
+		Frog_5.y = '433.1';
+		Score++;
+		sound[1].play();
+	} else {
+		Frog_5.x = FROG_5_POSX;
+		Frog_5.y = FROG_5_POSY;
+		Frog_5.scale.setTo(0.9);
+		sound[2].play();
+	}
+	Process();
+}
+
+function stopDrag_6() {
+	Frog_6.scale.setTo(0.7);
+	if (Frog_6.y > 86 && Frog_6.y < 189 && Frog_6.x > 252 && Frog_6.x < 500) {
+		Frog_6.inputEnabled = false;
+		Frog_6.x = '399.9';
+		Frog_6.y = '170.9';
+		Score++;
+		sound[1].play();
+	} else {
+		Frog_6.x = FROG_6_POSX;
+		Frog_6.y = FROG_6_POSY;
+		Frog_6.scale.setTo(0.9);
+		sound[2].play();
+	}
+	Process();
+}
+
+function stopDrag_7() {
+	Frog_7.scale.setTo(0.7);
+	if (Frog_7.y > 223 && Frog_7.y < 342 && Frog_7.x > 508 && Frog_7.x < 742) {
+		Frog_7.inputEnabled = false;
+		Frog_7.x = '630.6';
+		Frog_7.y = '313.7';
+		Score++;
+		sound[1].play();
+	} else {
+		Frog_7.x = FROG_7_POSX;
+		Frog_7.y = FROG_7_POSY;
+		Frog_7.scale.setTo(0.9);
+		sound[2].play();
+	}
+	Process();
+}
 
 function Process() {
-	if (Score == 4) {
+	if (Score == 7) {
 		game.time.events.add(Phaser.Timer.SECOND * 1, Delay1, this);
-		var String_1 = 'tuyệt vời , bạn giỏi lắm'
-		var info = game.add.text(100, 30, String_1, style);
 		button = game.add.button(1200, 650, 'next', Process2, this, 2, 1, 0);
 		button.scale.setTo(1.2);
-		game.time.events.add(Phaser.Timer.SECOND * 1000, Process2, this);
+		setTimeout(function () {
+			Process2();
+		}, 2000);
 	}
 }
+
 function Delay1() {
 	game.add.tween(sound[3].play()).to(2000, Phaser.Easing.Linear.None, true);
 }
+
 function Process2() {
-	game.add.tween(winx = window.location = "/api").to(
-		{ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
-
+	setTimeout(function () {
+		window.location = "/api";
+	}, 2000);
 }
-
-
 
 function update() {
 	game.scale.forceOrientation(false, true);
 	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	//this.scale.setScreenSize( true );		
 }
+
 function render() {
 	//game.debug.spriteInfo(Score,32,32);
-	//game.debug.spriteInfo(Basket_4, 32, 32);
+	//game.debug.spriteInfo(Frog_1, 32, 32);
 }
